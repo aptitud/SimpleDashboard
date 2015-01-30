2e7575ef296d00e294d3c464ce8256160237d11a
Andreas Nilsson <amidrunk@gmail.com>
2015-01-30 15:09:29 +0100

    Added trello cache

--- index.js
+++ index.js
@@ -12,7 +12,26 @@ app.get('/', function(request, response) {
 });
 
 app.get("/consultants", function(request, response) {
-  Trello.retrieveConsultants(function(consultants) {
+  var getConsultants = function(callback) {
+    if (this.__cache && Date.now() - this.__cacheDate > 5 * 60 * 1000) {
+      delete this.__cache;
+    }
+
+    if (this.__cache) {
+      callback(this.__cache);
+    } else {
+      var self = this;
+
+      Trello.retrieveConsultants(function(consultants) {
+        self.__cache = consultants;
+        self.__cacheDate = Date.now();
+
+        callback(consultants);
+      });
+    }
+  };
+
+  getConsultants(function(consultants) {
     response.writeHead(200, {"Content-Type": "application/json; charset=utf-8", "Content-Encoding": "utf-8"})
     response.end(JSON.stringify(consultants));
   });
