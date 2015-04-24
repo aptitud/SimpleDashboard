ef306f4d04996db4bd8bd26f6720dbb0e2cd4649
Marcus Nylander <marcus.nylander@aptitud.se>
2015-04-24 14:52:26 +0200

    First try to redirect to https

--- index.js
+++ index.js
@@ -7,36 +7,51 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.redirect("/public/index.html");
+function secureToHttps(request, response) {
+    var protocol = request.headers['X-Forwarded-Proto'];
+    if (protocol && protocol == 'http') {
+        response.redirect("https://" + req.hostname + "/public/index.html");
+        return true;
+    }
+    return false;
+}
+
+app.get('/', function (request, response) {
+    if (secureToHttps(request, response)) {
+        return;
+    }
+    response.redirect("/public/index.html");
 });
 
-app.get("/consultants", function(request, response) {
-  var getConsultants = function(callback) {
-    if (this.__cache && Date.now() - this.__cacheDate > 5 * 60 * 1000) {
-      delete this.__cache;
+app.get("/consultants", function (request, response) {
+    if (secureToHttps(request, response)) {
+        return;
     }
+    var getConsultants = function (callback) {
+        if (this.__cache && Date.now() - this.__cacheDate > 5 * 60 * 1000) {
+            delete this.__cache;
+        }
 
-    if (this.__cache) {
-      callback(this.__cache);
-    } else {
-      var self = this;
+        if (this.__cache) {
+            callback(this.__cache);
+        } else {
+            var self = this;
 
-      Trello.retrieveConsultants(function(consultants) {
-        self.__cache = consultants;
-        self.__cacheDate = Date.now();
+            Trello.retrieveConsultants(function (consultants) {
+                self.__cache = consultants;
+                self.__cacheDate = Date.now();
 
-        callback(consultants);
-      });
-    }
-  };
+                callback(consultants);
+            });
+        }
+    };
 
-  getConsultants(function(consultants) {
-    response.writeHead(200, {"Content-Type": "application/json; charset=utf-8", "Content-Encoding": "utf-8"})
-    response.end(JSON.stringify(consultants));
-  });
+    getConsultants(function (consultants) {
+        response.writeHead(200, {"Content-Type": "application/json; charset=utf-8", "Content-Encoding": "utf-8"})
+        response.end(JSON.stringify(consultants));
+    });
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+app.listen(app.get('port'), function () {
+    console.log("Node app is running at localhost:" + app.get('port'));
 });
