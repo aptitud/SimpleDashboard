e29ef28ba0f7d9d97b3f9452e743d22f20811ac0
Magnus Hübner <magnuh@users.noreply.github.com>
2021-11-19 15:10:35 +0100

    Displays employees on map based on addresses from Trello.

--- src/maps-helper.js
+++ src/maps-helper.js
@@ -4,21 +4,24 @@ const config = {
     KEY: 'AIzaSyDwChmJ8Y45tMxQ27HNhLyc7SeohwRqIT8',
 };
 
-module.exports.getPositionsFromAddresses = async (employees) => {
-    await employees.forEach((employee, i) => {
-        if(employee.address && i === 0) {
-            axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(employee.address)}&key=${config.KEY}`).then((result) => {
-                console.log(result.data.results[0]);
-                
-                if(result.data?.results[0]?.geometry?.location) {
-                    console.log(result.data.results[0].geometry.location)
-                        employee.position = result.data.results[0].geometry.location
-                }
-            }).catch((error) => {
-                console.log(error);
-            });
-        }
-    });
+module.exports.getPositionsFromAddresses = async (employees = []) => {
+	const promises = [];
+	const employeesWithAddress = employees.filter((employee) => employee.address);
 
-    return employees;
+	employeesWithAddress.forEach((employee) => {
+		promises.push(axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(employee.address)}&key=${config.KEY}`));
+	});
+
+	await Promise.all(promises).then((results) => {
+		results.forEach((result, i) => {
+			if(result.data?.results[0]?.geometry?.location) {
+				employeesWithAddress[i].position = result.data.results[0].geometry.location;
+			}
+		});
+
+	}).catch((error) => {
+		console.log(error);
+	});
+
+	return employees;
 }
